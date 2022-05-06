c9f8bdc1ebe2318868333a85fc526ecbfa2846db
ykang2020 <yk1123@vip.163.com>
2022-05-06 13:48:28 +0800

    test husy

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -2,6 +2,7 @@ const msg = require("fs").readFileSync(".git/COMMIT_EDITMSG", "utf-8").trim();
 
 const commitRE =
   /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/;
+  
 const mergeRe = /^(Merge pull request|Merge branch)/;
 
 if (!commitRE.test(msg)) {
